d61d92348dd232cf24c1558f2c29d1c8f073d2eb
vahea95 <vaheazizyan12@gmail.com>
2023-08-03 18:05:07 +0400

    Creating header component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Input} from "./Components/Input";
+import {Header} from "./Components/header";
 
 function App() {
   return (
     <div className="App">
       <Input/>
+        <Header/>
     </div>
   );
 }
---
+++ src/Components/header.tsx
@@ -0,0 +1,9 @@
+import chatIcon from '../icons/chatiIcon/chatIcon.svg'
+export const Header = () => {
+    return (
+        <div className="header">
+            <img src={chatIcon}/>
+            <p>Great Project</p>
+        </div>
+    )
+}
